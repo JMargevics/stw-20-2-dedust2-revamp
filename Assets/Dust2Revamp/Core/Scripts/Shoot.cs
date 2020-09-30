25a490cb8b6c405bbcdd4ba5a1edb29b63dcbd4d
Tomas Kiniulis <tomas.kiniulis@unity3d.com>
2020-09-30 17:30:58 +0300

    Adding first iteration of muzzle flash and vfx samples

--- Assets/Dust2Revamp/Core/Scripts/Shoot.cs
+++ Assets/Dust2Revamp/Core/Scripts/Shoot.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 [RequireComponent(typeof(PlayerManager))]
 public class Shoot : MonoBehaviour
@@ -12,6 +13,7 @@ public class Shoot : MonoBehaviour
     public float weaponRange = 50f;
     public float hitForce = 100f;
     public Transform gunEnd;
+    private Light muzzleLight;
 
     public Camera fpsCam;
     private WaitForSeconds shotDuration = new WaitForSeconds(0.07f);
@@ -21,9 +23,13 @@ public class Shoot : MonoBehaviour
     public AudioClip shoot;
     public AudioClip reload;
 
+    public VisualEffect muzzleFlash;
+
     void Start()
     {
         playerManager = GetComponent<PlayerManager>();
+        muzzleLight = gunEnd.GetComponent<Light>();
+        muzzleLight.enabled = false;
     }
 
 
@@ -31,9 +37,6 @@ public class Shoot : MonoBehaviour
     {
         if (Input.GetButton("Fire1") && Time.time > nextFire)
         {
-            playerManager.audioSource.clip = shoot;
-            playerManager.audioSource.Play();
-            playerManager.animator.SetTrigger("Shoot");
 
             nextFire = Time.time + fireRate;
 
@@ -61,7 +64,7 @@ public class Shoot : MonoBehaviour
                 {
                     hit.rigidbody.AddForce(-hit.normal * hitForce);
                 }
-                if(hit.transform.gameObject)
+                if (hit.transform.gameObject)
                 {
 
                 }
@@ -74,8 +77,15 @@ public class Shoot : MonoBehaviour
 
     private IEnumerator ShotEffect()
     {
-        //gunAudio.Play();
+        muzzleFlash.Play();
+        muzzleLight.enabled = true;
+        playerManager.animator.SetTrigger("Shoot");
+        playerManager.audioSource.clip = shoot;
+        playerManager.audioSource.Play();
 
         yield return shotDuration;
+
+        yield return new WaitForSeconds(0.005f);
+        muzzleLight.enabled = false;
     }
 }
